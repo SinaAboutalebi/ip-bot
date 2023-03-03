262b5e661e8b184b3a436299596883f5f239d546
Zer0Power <zeroopoower@gmail.com>
2023-03-03 18:41:02 +0330

    fix: intents

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
     ]
 });
